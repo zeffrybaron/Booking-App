05ddad63cc22000a4ba4cce3b40e168042df2998
Zeffry Baron Alafanta <zefrrybaron@gmail.com>
2022-11-16 22:04:01 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/home/Home'
 import List from './pages/listPages/List';
 import Hotel from './pages/hotel/Hotel'
-import './App.css';
+
 
 function App() {
   return (
