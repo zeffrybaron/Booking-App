3dd6fdf37d552cf624a1ea55ae42e8b0b925d2ab
Zeffry Baron Alafanta <zefrrybaron@gmail.com>
2022-11-16 23:06:48 +0700

    updata

--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -4,12 +4,7 @@ import Header from "../../components/header/Header";
 import MailList from "../../components/mailList/MailList";
 import Footer from "../../components/footer/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleArrowLeft,
-  faCircleArrowRight,
-  faCircleXmark,
-  faLocationDot,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCircleArrowLeft, faCircleArrowRight, faCircleXmark, faLocationDot,} from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 const Hotel = () => {
